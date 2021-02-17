0920a741a9ed8110f5c3cd9cb4438d8edcf22dfb
somesh122 <79034763+somesh122@users.noreply.github.com>
2021-02-17 20:20:54 +0530

    Add files via upload

---
+++ odd_even.py
@@ -0,0 +1,7 @@
+num = input("What num are you thinking?")
+num1=int(num)
+if num1%2==0 :
+    print("Thats an even number! Have another?")
+else:
+    print("Thats an odd number! Have another?")
+print(num1)
