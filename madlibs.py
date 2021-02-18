38220a0aa69b3929ced05992287eae5f7d6f24d3
Somesh Agrawal <79034763+somesh122@users.noreply.github.com>
2021-02-18 09:53:07 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+loop = 1
+while loop<9:
+
+    Food   = input('Enter a type of food: ')
+    Name   = input('Enter a girls name: ')
+    Adj    = input('Enter the adj:')
+    Noun   = input('Enter the noun: ')
+    Verb1  = input('Enter the verb in past tense:')
+    Verb2  = input('Enter the verb in past tense:')
+    Verb3  = input('Enter the verb in past tense:')
+    Noun1  = input('Enter a noun: ')
+    print('It was'+' '+ Food +' '+'day at school,and'+' '+Name+' '+'was super'+' '+ Adj+' '+'for lunch,But when she went outside to eat,a'+' '+Noun+' '+'stole her'+' '+Food+''+'!'+''+Name+' '+'chased the'+' '+Noun+' '+'all over school.She'+' '+Verb1 +' '+ ',' +' '+ Verb2 +',and' +' '+Verb3+' '+ 'through the playground.Then she tripped on her' +' '+ Noun1 +' '+ 'and the' +' '+ Noun +' '+ 'escaped!Luckily,'+' '+Name+' '+ 's friends were willing to share their'+' '+ Food+' '+ 'with her.')
+
+loop = loop+1
